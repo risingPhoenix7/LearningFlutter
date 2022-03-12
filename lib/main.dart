e2092c9409a127dd0ca9afaa476502a5af71e3e3
risingPhoenix7 <achinthyahebbar@gmail.com>
2022-03-12 15:16:20 +0530

    Image instead of text

--- lib/main.dart
+++ lib/main.dart
@@ -8,29 +8,21 @@ void main() {
 //to create this type stless and itll autofill, just type the name and put the class name as home:classname() in main, return scaffold
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
-
+//override meaning is that build over here overrides the build defined in parent class Stateless Widget
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('my first app'),
+        title: Text('my first app in flutter'),
         backgroundColor: Color(0xFFF44336),
         // or backgroundColor: Colors.red[500] red of strength 500 out of 1000
         centerTitle: true,
       ),
       body:Center(
-        child:Text(
-          'hello ninjas',
-          style: TextStyle(
-            //ctrl q on the textStyle gives all the properties it can have
-              fontSize:40.0,
-              fontStyle: FontStyle.italic,
-              color: Colors.grey,
-              fontFamily: 'IndieFlower'
+        child:Image(
+          image:NetworkImage('https://image.shutterstock.com/image-illustration/phoenix-journey-space-art-260nw-1405088537.jpg'),
 
-
-          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
