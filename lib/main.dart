efc516bc117977ab80df04d3984c1c758fa8c8f2
risingPhoenix7 <achinthyahebbar@gmail.com>
2022-03-15 23:58:00 +0530

    Adding a column to enclose all the previous widgets

--- lib/main.dart
+++ lib/main.dart
@@ -22,73 +22,75 @@ class Home extends StatelessWidget {
       ),
       /* If you want an image,
       body:Center(
-        child:Image(
-          image:NetworkImage('https://image.shutterstock.com/image-illustration/phoenix-journey-space-art-260nw-1405088537.jpg'),
-          ),
+        child:
       ),*/
 
-      //IconButton
-      /*body: Center(
-        child : IconButton(
-          onPressed: (){},
-          icon:Icon(Icons.airport_shuttle),
-          color: Colors.blue,
-          iconSize: 400,
-        )
-        )
-*/
-        /*body: Center(
-            child : ElevatedButton(
-              onPressed: (){
-                print('you clicked me');
-              },
-              child:Icon(
+      //If it has a child widget it restricts itself to the size of that widget
+
+      body:Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+
+
+        Container(
+        //height:260,
+        padding:EdgeInsets.fromLTRB(10, 10, 10, 10),//container and its inside
+        //margin: EdgeInsets.all(150),//container and its outside
+        child:Text('Container with text child',
+          style: TextStyle(fontSize: 50)
+        ),
+        color:Colors.purple,
+        ),
+          RaisedButton.icon(
+            onPressed: (){
+              print('raised button');
+            },
+            color: Colors.amber,
+            icon:Icon(
                 Icons.airport_shuttle,
-                    size:250,
+                size:80,
                 color:Colors.black
 
-              ),
-                style:ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
-                ),
-            )
-        )*/
-        /*body: Center(
-            child : RaisedButton.icon(
-              onPressed: (){
-                print('you clicked me');
-              },
-              color: Colors.amber,
-              icon:Icon(
-                  Icons.airport_shuttle,
-                  size:80,
-                  color:Colors.black
-
-              ),
-              label:Text(
-                  'Hi there',
+            ),
+            label:Text(
+              'Raised button',
               style: TextStyle(
-                fontSize: 50
+                  fontSize: 25
               ),
-              ),
-                textColor: Colors.green,
-              hoverColor: Colors.black,
-            )
-        )*/
-      //If it has a child widget it restricts itself to the size of that widget
-      body:Container(
-        height:260,
-        padding:EdgeInsets.fromLTRB(10, 10, 10, 10),//container and its inside
-        margin: EdgeInsets.all(150),//container and its outside
-        child:Text('Hi hello',
-        style: TextStyle(
-          fontSize: 50
-        ),),
-        color:Colors.purple,
-       //margin:,
-        //padding:,
-      ),
-      );
+            ),
+            textColor: Colors.green,
+            hoverColor: Colors.black,
+          ),
+
+          Image(
+            image:NetworkImage('https://image.shutterstock.com/image-illustration/phoenix-journey-space-art-260nw-1405088537.jpg'),
+          ),
+
+          ElevatedButton(
+            onPressed: (){
+              print('elevated button');
+            },
+            child:Icon(
+                Icons.add_to_queue,
+                size:150,
+                color:Colors.black
+
+            ),
+            style:ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+            ),
+          ),
+
+          IconButton(
+            onPressed: (){},
+            icon:Icon(Icons.airport_shuttle),
+            color: Colors.blue,
+            iconSize: 40,
+          )
+
+        ]
+
+      ));
 
     //Scaffold is like a wrapper to lots of widgets like appBar.body.bottomNavigationBar
     //bottomSheet,drawer,;
